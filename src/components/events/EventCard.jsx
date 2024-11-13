4e2e79cf72783f9b8300a058f4157c28a5977dc5
Enis <enissemerci2@gmail.com>
2024-11-13 15:54:14 +0300

    denmee

--- src/components/events/EventCard.jsx
+++ src/components/events/EventCard.jsx
@@ -26,12 +26,12 @@ export const EventCard = ({ event }) => {
         <CardMedia
           component="img"
           height="200"
-          image={event.imageUrl}  
+          image={event.imageUrl || "default_image_url"}  // Görsel yoksa varsayılan bir görsel koyabilirsiniz
           alt={event.name}
         />
         <CardContent>
           <Typography variant="h6" component="div">
-            {event.name}  // API'den dönen name
+            {event.name}  
           </Typography>
         </CardContent>
       </Card>
@@ -43,17 +43,19 @@ export const EventCard = ({ event }) => {
           <CardMedia
             component="img"
             height="200"
-            image={event.imageUrl}
+            image={event.imageUrl || "default_image_url"}
             alt={event.name}
             style={{ marginBottom: 16 }}
           />
           <Typography gutterBottom>{event.description}</Typography>
-          <Typography gutterBottom>Başlama Tarihi: {event.startDate}</Typography>
-          <Typography gutterBottom>Bitiş Tarihi: {event.endDate}</Typography>
+          <Typography gutterBottom>Başlama Tarihi: {new Date(event.startDate).toLocaleString()}</Typography>
+          <Typography gutterBottom>Bitiş Tarihi: {new Date(event.endDate).toLocaleString()}</Typography>
           <Typography gutterBottom>Konum: {event.location}</Typography>
           <Typography gutterBottom>Şehir: {event.city}</Typography>
           <Typography gutterBottom>Ülke: {event.country}</Typography>
-          <Typography gutterBottom>Bilet Fiyatı: {event.ticketPrice}</Typography>
+          <Typography gutterBottom>
+            Bilet Fiyatı: {event.isFree ? "Ücretsiz" : `${event.minPrice} - ${event.maxPrice} TL`}
+          </Typography>
         </DialogContent>
       </Dialog>
     </>
